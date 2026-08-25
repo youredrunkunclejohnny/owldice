21842ffc0cf30a37ca086e61e03b3fb4938d8fd5
youredrunkunclejohnny <youredrunkunclejohnny@gmail.com>
2026-08-24 19:00:17 -0700

    Update diceSets.ts
    Commented out non D6

--- src/sets/diceSets.ts
+++ src/sets/diceSets.ts
@@ -30,13 +30,13 @@ function createStandardSet(style: DiceStyle): DiceSet {
     id,
     name: `${style.toLowerCase()} dice`,
     dice: [
-      { id: `${id}_D4`, type: "D4", style },
+/*      { id: `${id}_D4`, type: "D4", style }, */
       { id: `${id}_D6`, type: "D6", style },
-      { id: `${id}_D8`, type: "D8", style },
+/*      { id: `${id}_D8`, type: "D8", style },
       { id: `${id}_D10`, type: "D10", style },
       { id: `${id}_D12`, type: "D12", style },
       { id: `${id}_D20`, type: "D20", style },
-      { id: `${id}_D100`, type: "D100", style },
+      { id: `${id}_D100`, type: "D100", style }, */
     ],
     previewImage: standardPreviews[style],
   };
